ebebc71f296a676357754245c2b61151b3ba3a43
Sumeet Shirgure <sumeetshirgure@gmail.com>
2025-11-29 08:24:39 -0500

    Restructure directories.

---
+++ dynamarq/benchmarks/__init__.py
@@ -0,0 +1,20 @@
+from .states import (
+        GHZ,
+        GHZReset,
+    )
+
+from .gates import (
+        CNOTLadder,
+        Fanout,
+        LongRangeCNOT,
+        LongRangeCNOTSparse,
+    )
+
+__all__ = [
+        "GHZ",
+        "GHZReset",
+         "CNOTLadder",
+        "Fanout",
+        "LongRangeCNOT",
+        "LongRangeCNOTSparse",
+        ]
