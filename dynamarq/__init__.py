97dc4aac7fbef08ae93c8706b56028b8b595994d
Sumeet Shirgure <sumeetshirgure@gmail.com>
2025-11-28 19:33:00 -0500

    Add LongRangeCNOTSparse benchmark.

--- dynamarq/__init__.py
+++ dynamarq/__init__.py
@@ -6,6 +6,7 @@ from .benchmarks import (
         CNOTLadder,
         Fanout,
         LongRangeCNOT,
+        LongRangeCNOTSparse,
         )
 
 __all__ = [
@@ -14,4 +15,5 @@ __all__ = [
         "CNOTLadder",
         "Fanout",
         "LongRangeCNOT",
+        "LongRangeCNOTSparse",
         ]
