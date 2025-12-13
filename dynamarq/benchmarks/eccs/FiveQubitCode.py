48ea4f96404e58d46bbf3a90120082f3da247ff8
Sumeet Shirgure <sumeetshirgure@gmail.com>
2025-12-13 10:38:34 -0500

    Use logical error rate for FiveQubitCode.

--- dynamarq/benchmarks/eccs/FiveQubitCode.py
+++ dynamarq/benchmarks/eccs/FiveQubitCode.py
@@ -21,25 +21,10 @@ class FiveQubitCode(Benchmark) :
     encoded in the five qubit XZZXI code.
     Performs one round of syndrome measurement and Z logical stabilizer measurement.
 
-    We evaluate the Hellinger fidelity between the obtained distribution and the
-    ideal distribution (depending on the initial state) as the score for this benchmark.
+    We evaluate the logical error rate as the score for this benchmark.
     """
     def __init__(self) :
         self.init_states = ['0', '1']
-
-        # Taken from https://en.wikipedia.org/wiki/Five-qubit_error_correcting_code#Encoding
-        self.ket0 = {basis: 1.0/16 for basis in [
-            "00000", "10010", "01001", "10100",
-            "01010", "11011", "00110", "11000",
-            "11101", "00011", "11110", "01111",
-            "10001", "01100", "10111", "00101",
-            ]}
-        self.ket1 = {basis: 1.0/16 for basis in [
-            "11111", "01101", "10110", "01011",
-            "10101", "00100", "11001", "00111",
-            "00010", "11100", "00001", "10000",
-            "01110", "10011", "01000", "11010",
-            ]}
 
 
     def name(self) :
@@ -53,11 +38,13 @@ class FiveQubitCode(Benchmark) :
             data = QuantumRegister(5, 'data')
             anc  = QuantumRegister(4, 'anc')
             stab = QuantumRegister(1, 'stab')
+            log  = QuantumRegister(1, 'log')
+            result = ClassicalRegister(1, 'result')
             meas = ClassicalRegister(5, 'meas')
             syn  = ClassicalRegister(4, 'syn')
-            op   = ClassicalRegister(1, 'op')
+            op = ClassicalRegister(1, 'op')
 
-            circuit = QuantumCircuit(data, anc, stab, meas, syn, op)
+            circuit = QuantumCircuit(data, anc, stab, log, result, meas, syn, op)
 
             # Perform syndrome measurement.
             circuit.barrier()
@@ -184,6 +171,14 @@ class FiveQubitCode(Benchmark) :
 
             circuit.barrier()
 
+            circuit.h(log[0])
+
+            for i in range(5) :
+                circuit.cz(log[0], data[i])
+
+            circuit.h(log[0])
+
+            circuit.measure(log, result)
             circuit.measure(data, meas)
 
             circuits.append(circuit)
@@ -192,21 +187,20 @@ class FiveQubitCode(Benchmark) :
 
 
     def qiskit_score(self, counts_list) :
-        ideal_dists = [self.ket0, self.ket1]
 
         fidelity_sum = 0.0
-        for ideal_dist, counts in zip(ideal_dists, counts_list) :
+        for init_state, counts in zip(self.init_states, counts_list) :
             total_shots = sum(counts.values())
 
             device_hist = dict()
-            for bitstr, count in counts.items() :
-                data_qubits = bitstr[-1:-1-5:-1]
-                if data_qubits not in device_hist :
-                    device_hist[data_qubits] = 0
-                device_hist[data_qubits] += count
-            device_dist = {bitstring: count/total_shots for bitstring, count in device_hist.items()}
+            for bitstring, count in counts.items() :
+                bit = bitstring[-1]
+                if bit not in device_hist :
+                    device_hist[bit] = 0
+                device_hist[bit] += count
+            device_dist = {bit: count / total_shots for bit, count in device_hist.items()}
 
-            fidelity_sum += hellinger_fidelity(ideal_dist, device_dist)
+            fidelity_sum += device_dist[init_state]
 
         return fidelity_sum / len(counts_list)
 
@@ -217,6 +211,7 @@ class FiveQubitCode(Benchmark) :
         def five_qubit_code(data: array[qubit, 5] @owned,
                             anc: array[qubit, 4] @owned,
                             stab: qubit @owned,
+                            result_qubit: qubit @owned,
                             init_state: bool, # False for |0>, True for |1>
                             ) -> None :
 
@@ -301,25 +296,35 @@ class FiveQubitCode(Benchmark) :
                 for i in range(5) :
                     x(data[i])
 
+            h(result_qubit)
+
+            for i in range(5) :
+                cz(result_qubit, data[i])
+
+            h(result_qubit)
+
+            res = measure(result_qubit)
             meas = measure_array(data)
 
-            for v in meas : result('meas', v)
+            result('result', res)
 
         @guppy
         def fqc0() -> None :
             data = array(qubit() for _ in range(5))
             anc  = array(qubit() for _ in range(4))
             stab = qubit()
+            result_qubit = qubit()
             init_state = False
-            five_qubit_code(data, anc, stab, init_state)
+            five_qubit_code(data, anc, stab, result_qubit, init_state)
 
         @guppy
         def fqc1() -> None :
             data = array(qubit() for _ in range(5))
             anc  = array(qubit() for _ in range(4))
             stab = qubit()
+            result_qubit = qubit()
             init_state = True
-            five_qubit_code(data, anc, stab, init_state)
+            five_qubit_code(data, anc, stab, result_qubit, init_state)
 
         return [fqc0, fqc1]
 
@@ -327,11 +332,10 @@ class FiveQubitCode(Benchmark) :
 
 
     def guppy_score(self, results_list) :
-        ideal_dists = [self.ket0, self.ket1]
 
         fidelity_sum = 0.0
 
-        for ideal_dist, results in zip(ideal_dists, results_list) :
+        for init_state, results in zip(self.init_states, results_list) :
             collated_counts = results.collated_counts()
             total_shots = sum(collated_counts.values())
 
@@ -339,13 +343,13 @@ class FiveQubitCode(Benchmark) :
 
             for key in collated_counts.keys() :
                 string = key[0][1]
-                freq = collated_counts[ (('meas', string),) ]
+                freq = collated_counts[ (('result', string),) ]
                 if string not in device_hist :
                     device_hist[ string ] = 0
                 device_hist[ string ] += freq
 
             device_dist = {bitstring: count/total_shots for bitstring, count in device_hist.items()}
 
-            fidelity_sum += hellinger_fidelity(ideal_dist, device_dist)
+            fidelity_sum += device_dist[init_state]
 
         return fidelity_sum / len(results_list)
